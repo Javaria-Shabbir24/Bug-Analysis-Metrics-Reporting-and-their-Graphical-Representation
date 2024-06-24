6ca56dec1d39b43cb896d5ff7c133502eb65f412
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2024-06-24 15:31:17 +0500

    Task10

---
+++ Task10/ParetoChartForBugsStillNotClosed.py
@@ -0,0 +1,19 @@
+##import libraries
+#import pandas as pd # for dataframes 
+#import matplotlib.pyplot as plot # for graphs
+## read the excel file in dataframe
+#dataframe=pd.read_excel('dataset3.xlsx')
+##making a separate bundle of only Bugs rows by filtering the dataframe
+#Bugs= dataframe[(dataframe['type']=='Bug') & (dataframe['status'])!='Closed' ]
+##count of bugs after grouping the data on basis of assignee
+#BugsCount=Bugs['assignee'].value_counts()
+##sort the dataframe in descending order
+#BugsCount=BugsCount.sort_values(ascending=False)
+##create pareto chart
+#fig,ax1=plot.subplots()
+#ax1.bar(BugsCount.index,BugsCount.values,color='brown',alpha=0.6,width=1)
+#ax1.set_ylabel('Number of Bugs with Status Not Closed')
+#ax1.set_xlabel('Assignee')
+#plot.title('Pareto Chart for Bugs Given To each Assignee')
+#plot.tight_layout() # ensure all elements fit well
+#plot.show()
