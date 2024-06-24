60f7641719e646029d449f6a469908bf9da59623
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2024-06-24 15:31:35 +0500

    Task11

---
+++ Task11/ProportionandPercentageOfActualDefectsInDataset3.py
@@ -0,0 +1,52 @@
+#import pandas as pd
+##read excel file
+#dataframe = pd.read_excel('dataset3.xlsx')
+## selecting rows where the actual problem is bug
+#bugs = dataframe[dataframe['type'] == 'Bug']
+## Proportion of Bugs
+#ProportionOfBugs = len(bugs) / len(dataframe)
+## Percentage of Bugs
+#PercentageOfBugs = ProportionOfBugs * 100
+## Display the results
+#print("The Proportion of Actual Defects (Bugs) In Dataset 3: ",ProportionOfBugs)
+#print("The Percentage of Actual Defects (Bugs) In Dataset 3: ",PercentageOfBugs,"%")
+##module A
+#dataframe = pd.read_excel('dataset3.xlsx',sheet_name='A')
+#bugs = dataframe[dataframe['type'] == 'Bug']
+## Proportion of Bugs
+#ProportionOfBugs = len(bugs) / len(dataframe)
+## Percentage of Bugs
+#PercentageOfBugs = ProportionOfBugs * 100
+## Display the results
+#print("The Proportion of Actual Defects (Bugs) In Module A: ",ProportionOfBugs)
+#print("The Percentage of Actual Defects (Bugs) In Module A: ",PercentageOfBugs,"%")
+##module C
+#dataframe = pd.read_excel('dataset3.xlsx',sheet_name='C')
+#bugs = dataframe[dataframe['type'] == 'Bug']
+## Proportion of Bugs
+#ProportionOfBugs = len(bugs) / len(dataframe)
+## Percentage of Bugs
+#PercentageOfBugs = ProportionOfBugs * 100
+## Display the results
+#print("The Proportion of Actual Defects (Bugs) In Module C: ",ProportionOfBugs)
+#print("The Percentage of Actual Defects (Bugs) In Module C: ",PercentageOfBugs,"%")
+##module C
+#dataframe = pd.read_excel('dataset3.xlsx',sheet_name='D')
+#bugs = dataframe[dataframe['type'] == 'Bug']
+## Proportion of Bugs
+#ProportionOfBugs = len(bugs) / len(dataframe)
+## Percentage of Bugs
+#PercentageOfBugs = ProportionOfBugs * 100
+## Display the results
+#print("The Proportion of Actual Defects (Bugs) In Module D: ",ProportionOfBugs)
+#print("The Percentage of Actual Defects (Bugs) In Module D: ",PercentageOfBugs,"%")
+##module W
+#dataframe = pd.read_excel('dataset3.xlsx',sheet_name='W')
+#bugs = dataframe[dataframe['type'] == 'Bug']
+## Proportion of Bugs
+#ProportionOfBugs = len(bugs) / len(dataframe)
+## Percentage of Bugs
+#PercentageOfBugs = ProportionOfBugs * 100
+## Display the results
+#print("The Proportion of Actual Defects (Bugs) In Module W: ",ProportionOfBugs)
+#print("The Percentage of Actual Defects (Bugs) In Module W: ",PercentageOfBugs,"%")
